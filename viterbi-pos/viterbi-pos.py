1c94941eaa23ad7a0f5d27210b4ef7dbfaaae1f7
suyash0103 <suyashghuge@gmail.com>
2018-03-31 18:21:16 +0530

    word list, tag list

--- viterbi-pos/viterbi-pos.py
+++ viterbi-pos/viterbi-pos.py
@@ -8,3 +8,15 @@ training_str = training_file.read()
 training_data = training_str.split()
 print (training_data)
 
+train_words = ['']
+train_tags = ['']
+
+training_data_size = len(training_data)
+train_words *= training_data_size
+train_tags *= training_data_size
+
+for i in range(training_data_size):
+    word_tag = training_data[i].split('/')
+    train_words[i] = word_tag[0]
+    train_tags[i] = word_tag[1]
+    print (i, train_words[i], train_tags[i])

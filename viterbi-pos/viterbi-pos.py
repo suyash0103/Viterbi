67039c9afaf88c5cf50fd8a19da030a8bb5b6fa6
suyash0103 <suyashghuge@gmail.com>
2018-03-31 22:23:33 +0530

    transition probability dictionary filled

--- viterbi-pos/viterbi-pos.py
+++ viterbi-pos/viterbi-pos.py
@@ -21,9 +21,21 @@ for i in range(0, training_data_size):
     train_tags[i] = word_tag[1]
     # print (i, train_words[i], train_tags[i])
 
-
 # TRANSITION PRABABILITY
+""" Dictionary stores keys and values in the form : { tagA : {tagB : number of times tagB follows tagA} } """
 transition_prob = {}
 
 # EMISSION PROBABILITY
+""" Dictionary stores keys and values in the form : { wordA : {tagA : number of times wordA is tagged with tagA} } """
 emission_prob = {}
+
+for i in range(training_data_size - 1):
+    # For Dictionary of Transition Probability
+    outer_key = train_tags[i]
+    inner_key = train_tags[i + 1]
+    # If outer_key exists as a key, return its value, or else return an empty dictionary
+    transition_prob[outer_key] = transition_prob.get(outer_key, {})
+    # If inner_key exists for this particular outer_key, return value of inner dictionary, or else return 0
+    transition_prob[outer_key][inner_key] = transition_prob[outer_key].get(inner_key, 0)
+    # Increment count
+    transition_prob[outer_key][inner_key] += 1

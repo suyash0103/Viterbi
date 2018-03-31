b2251e7f6756b73971796a5d2e1e5c3c03f24672
suyash0103 <suyashghuge@gmail.com>
2018-03-31 18:31:05 +0530

    lists working

--- viterbi-pos/viterbi-pos.py
+++ viterbi-pos/viterbi-pos.py
@@ -15,8 +15,8 @@ training_data_size = len(training_data)
 train_words *= training_data_size
 train_tags *= training_data_size
 
-for i in range(training_data_size):
+for i in range(0, training_data_size):
     word_tag = training_data[i].split('/')
     train_words[i] = word_tag[0]
     train_tags[i] = word_tag[1]
-    print (i, train_words[i], train_tags[i])
+    # print (i, train_words[i], train_tags[i])
